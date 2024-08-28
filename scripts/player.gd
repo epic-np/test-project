fc92f83e156de98a35fb5107858322c5cbf84813
epic-np <epicninjapants04@gmail.com>
2024-08-28 16:23:13 -0500

    Initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+const speed = 100
+
+func physics_process(delta):
+	player_movement(delta)
+
+func player_movement(_delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_up"):
+		velocity.x = 0
+		velocity.y = speed
+	elif Input.is_action_pressed("ui_down"):
+		velocity.x = 0
+		velocity.y = -speed
+	else:
+		velocity.x = 0
+		velocity.y = 0
+		
+	move_and_slide()
